4412bd2523fd30e6fde36433d30767d48d9772d6
topol <oleg10062005@mail.ru>
2025-05-15 10:06:40 +0300

    Novigation Bar + firebase 50/50

--- lib/features/crypto_list/view/crypto_list_screen.dart
+++ lib/features/crypto_list/view/crypto_list_screen.dart
@@ -15,10 +15,8 @@ class CryptoListScreen extends StatefulWidget {
 }
 
 class _CryptoListScreenState extends State<CryptoListScreen> {
-
-
   final _cryptoListBloc = CryptoListBloc(GetIt.I<AbstractCoinsRepository>());
-
+  int currentPageIndex = 0;
 
   @override
   void initState() {
@@ -28,68 +26,89 @@ class _CryptoListScreenState extends State<CryptoListScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        //backgroundColor: Colors.yellow.shade500,
         title: Text(widget.title),
       ),
-      body: RefreshIndicator(
-        onRefresh: () async {
-          _cryptoListBloc.add(LoadCryptoList());
+      body: _buildCurrentScreen(), // Переключаем экраны здесь
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
         },
-        child: BlocBuilder<CryptoListBloc, CryptoListState> (
-          bloc: _cryptoListBloc,
-          builder: (
-              BuildContext context,
-              CryptoListState state) {
-            if (state is CryptoListLoaded) {
-                    //const Center (child: CircularProgressIndicator())
-                    return ListView.separated(
-                    padding: EdgeInsets.only(top: 2, left: 10, bottom: 50),
-                    itemCount: state.coinsList.length,
-                    separatorBuilder: (context, index) => Divider(),
-                    itemBuilder: (context, i) {
-                      final coin = state.coinsList[i];
-                      return CryptoCoinTile(coin: coin);
-                    }
+        backgroundColor: Colors.white12,
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.notifications_sharp),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.messenger_sharp),
+            label: 'Messages',
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Метод для выбора текущего экрана
+  Widget _buildCurrentScreen() {
+    switch (currentPageIndex) {
+      case 0: // Home (экран с криптовалютами)
+        return RefreshIndicator(
+          onRefresh: () async {
+            _cryptoListBloc.add(LoadCryptoList());
+          },
+          child: BlocBuilder<CryptoListBloc, CryptoListState>(
+            bloc: _cryptoListBloc,
+            builder: (context, state) {
+              if (state is CryptoListLoaded) {
+                return ListView.separated(
+                  padding: const EdgeInsets.only(top: 2, left: 10, bottom: 50),
+                  itemCount: state.coinsList.length,
+                  separatorBuilder: (context, index) => const Divider(),
+                  itemBuilder: (context, i) {
+                    final coin = state.coinsList[i];
+                    return CryptoCoinTile(coin: coin);
+                  },
                 );
-
-
-
-            }
-
-            if (state is CryptoListLoadingFailure) {
-              return Center(
+              }
+              if (state is CryptoListLoadingFailure) {
+                return Center(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(
-                        "OSHIBKA",
-                      ),
+                      const Text("ОШИБКА ЗАГРУЗКИ"),
                       const SizedBox(height: 35),
                       TextButton(
-                          onPressed: (){
-                            _cryptoListBloc.add(LoadCryptoList());
-                          },
-                          child: const Text("Rogaliki")
-                      )
+                        onPressed: () {
+                          _cryptoListBloc.add(LoadCryptoList());
+                        },
+                        child: const Text("Попробовать снова"),
+                      ),
                     ],
-                  )
-
-              );
-
-            }
-            return const Center (child: CircularProgressIndicator()); }, ),
-      )
-
-
-
-
-    //
-    //
-    );
+                  ),
+                );
+              }
+              return const Center(child: CircularProgressIndicator());
+            },
+          ),
+        );
+      case 1: // Notifications
+        return const Center(child: Text("Уведомления"));
+      case 2: // Messages
+        return const Center(child: Text("Сообщения"));
+      default:
+        return const Center(child: Text("Неизвестный экран"));
+    }
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:agregator_kripto/app.dart';
 import 'package:agregator_kripto/repositories/crypto_coins/abstract_coins_repository.dart';
 import 'package:agregator_kripto/repositories/crypto_coins/crypto_coins_repository.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   GetIt.I.registerLazySingleton<AbstractCoinsRepository> (() => CryptoCoinsRepository(dio: Dio()));
   runApp(const CryptoApp());
 }
