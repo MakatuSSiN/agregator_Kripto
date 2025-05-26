1ed9517a1dabc95fe4f962f0e7c86260728ce3a8
MakatuSSiN <malkovlev05@gmail.com>
2025-05-26 20:53:01 +0300

    ubrana nizh panel zhestov

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
 
@@ -18,6 +19,9 @@ final getIt = GetIt.instance;
 void main() async{
 
   WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
